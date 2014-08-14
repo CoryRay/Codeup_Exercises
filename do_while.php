958cbcf721a16d20365ddf3ddc9dcee82eedeb4e
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-14 10:25:35 -0500

    Second part of exercise

--- do_while.php
+++ do_while.php
@@ -1,10 +1,11 @@
 <?php
 
-$number = 0;
+$number = 100;
 
-do {
-	echo($number\n);
-	$number+=2;
-} while ($number <= 100);
+ do{
+	echo "$number\n";
+	$number-=5;
+} while ($number >= -10);
+
 
 ?>
