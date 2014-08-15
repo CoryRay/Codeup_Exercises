73d6bd81493c2546afc523f1e5f9d056864901a6
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-15 11:33:53 -0500

    finished exercises

--- foreach.php
+++ foreach.php
@@ -2,31 +2,36 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-foreach ($things as $value) {
-	if (is_int($value)) {
-		echo "$value is an integer!\n";
-	}
-	elseif (is_string($value)) {
-		echo "$value is a string!\n";
-	}
-	elseif (is_bool($value)) {
-		echo "\$value is a boolean!\n";
-	}
-	elseif (is_float($value)) {
-		echo "$value is a float!\n";
-	}
-	elseif (is_array($value)) {
+	foreach ($things as $value) {
+		if (is_int($value)) {
+			echo "$value is an integer!\n";
+		}
+		elseif (is_string($value)) {
+			echo "$value is a string!\n";
+		}
+		elseif (is_bool($value)) {
+			echo "\$value is a boolean!\n";
+		}
+		elseif (is_float($value)) {
+			echo "$value is a float!\n";
+		}
+		elseif (is_array($value)) {
 
-		echo "\$value is an array:\n";
+			echo "\$value is an array:\n";
 
-		foreach ($value as $arr) {
-			echo "$arr \n";
+			foreach ($value as $arr) {
+				echo "$arr \n";
+			}
+		}
+		elseif (is_null($value)) {
+			echo "\$value is NULL\n";
 		}
 	}
-	elseif (is_null($value)) {
-		echo "\$value is NULL\n";
+
+	foreach ($things as $value) {
+		if (is_scalar($value))
+			echo "$value is a scalar\n";	
 	}
-}
 
 
 ?>
