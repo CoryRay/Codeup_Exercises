1022529fa0e0797e95b4573ea5b06f7427ad55f3
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-25 16:53:35 -0500

    finished exercise

---
+++ hw_wk2_p2.php
@@ -0,0 +1,11 @@
+<?php
+
+function area_of_circle($radius) {
+	return pi() * pow($radius, 2);
+}
+
+function area_of_donut($outer, $inner) {
+	return area_of_circle($outer) - area_of_circle($inner);
+}
+
+echo area_of_donut(15, 5);
