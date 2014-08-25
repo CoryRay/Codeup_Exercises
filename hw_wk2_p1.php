1530688769f0fd1c0dc2252b1c899a68cc24a290
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-25 16:38:13 -0500

    added strtotime with help of frank, complete first exercise

--- hw_wk2_p1.php
+++ hw_wk2_p1.php
@@ -1,10 +1,11 @@
 <?php
 
 function days_of_month($month) {
-	date_date_set (date_create(), 2014, $month, 1);
-	$days = date('t' /*[ $timestamp = time()]*/);
-	$month = date('F' /*[ $timestamp = time()]*/);
+	// date_date_set (date_create(), 2014, $month, 1);
+	$stamp = strtotime($month);
+	$days = date('t', $stamp);
+	// $month = date('F' [ $timestamp = time()]);
 	echo "$month has $days days in it.";
 }
 
-days_of_month(3);
+days_of_month($argv[1]);
