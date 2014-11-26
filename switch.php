e14f71091d4ee7fcaf4dd3130eaae61e25541384
Cory Rodriguez <coreyreylp@gmail.com>
2014-11-26 11:24:42 -0600

    updated indentation

--- switch.php
+++ switch.php
@@ -9,26 +9,26 @@ $day_of_week = date('N');
 
 switch($day_of_week) {
     case 1:
-    echo ucfirst('Monday') . PHP_EOL;
-    break;
+        echo ucfirst('Monday') . PHP_EOL;
+        break;
     case 2:
-    echo ucfirst('tuesday') . PHP_EOL;
-    break;
+        echo ucfirst('tuesday') . PHP_EOL;
+        break;
     case 3:
-    echo ucfirst('wednesday') . PHP_EOL;
-    break;
+        echo ucfirst('wednesday') . PHP_EOL;
+        break;
     case 4:
-    echo ucfirst('thursday') . PHP_EOL;
-    break;
+        echo ucfirst('thursday') . PHP_EOL;
+        break;
     case 5:
-    echo ucfirst('Friday') . PHP_EOL;
-    break;
+        echo ucfirst('Friday') . PHP_EOL;
+        break;
     case 6:
-    echo ucfirst('saturday') . PHP_EOL;
-    break;
+        echo ucfirst('saturday') . PHP_EOL;
+        break;
     case 7:
-    echo ucfirst('sunday') . PHP_EOL;
-    break;
+        echo ucfirst('sunday') . PHP_EOL;
+        break;
 };
 
 /*if($day_of_week == 1) {
