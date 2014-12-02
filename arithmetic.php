ca5d66ff276506beb68f9f919ff56d54c0728f0d
Cory Rodriguez <coreyreylp@gmail.com>
2014-12-02 10:25:17 -0600

    cleaned up code

--- arithmetic.php
+++ arithmetic.php
@@ -5,56 +5,56 @@ $b = $argv[2];
 
 function error_numeric()
 {
-	echo "Error! Arguments must be a numeric value!" . PHP_EOL;
+    echo "Error! Arguments must be a numeric value!" . PHP_EOL;
 }
 
 function add($a, $b)
 {
     if (!is_numeric($a) || !is_numeric($b)) {
-		error_numeric();
-	} else {
-		return $a + $b . PHP_EOL;
-	}
+        error_numeric();
+    } else {
+        return $a + $b . PHP_EOL;
+    }
 }
 
 function subtract($a, $b)
 {
     if (!is_numeric($a) || !is_numeric($b)) {
-		error_numeric();
-	} else {
-		return $a - $b . PHP_EOL;
-	}
+        error_numeric();
+    } else {
+        return $a - $b . PHP_EOL;
+    }
 }
 
 function multiply($a, $b)
 {
     if (!is_numeric($a) || !is_numeric($b)) {
-		error_numeric();
-	} else {
-		return $a * $b . PHP_EOL;
-	}
+        error_numeric();
+    } else {
+        return $a * $b . PHP_EOL;
+    }
 }
 
 function divide($a, $b)
 {
-	if (!is_numeric($a) || !is_numeric($b)) {
-		error_numeric();
-	} elseif ($b == 0) {
-    	return FALSE;
-	} else {
-    	return $a / $b . PHP_EOL;
-	}
+    if (!is_numeric($a) || !is_numeric($b)) {
+        error_numeric();
+    } elseif ($b == 0) {
+        return FALSE;
+    } else {
+        return $a / $b . PHP_EOL;
+    }
 }
 
 function modulus($a, $b)
 {
-	if (!is_numeric($a) || !is_numeric($b)) {
-		error_numeric();
-	} elseif ($b == 0) {
-    	return FALSE;
-	} else {
-    	return $a % $b . PHP_EOL;
-	}
+    if (!is_numeric($a) || !is_numeric($b)) {
+        error_numeric();
+    } elseif ($b == 0) {
+        return FALSE;
+    } else {
+        return $a % $b . PHP_EOL;
+    }
 }
 
 echo add($a, $b);
--- do_while.php
+++ do_while.php
@@ -2,10 +2,9 @@
 
 $number = 2;
 
-do{
-	echo "$number\n";
-	$number*=$number;
+do {
+    echo "$number\n";
+    $number*=$number;
 } while ($number <= 1000000);
-
 
 ?>
